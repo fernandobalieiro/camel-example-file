c38ba396acadcff9ec23930108cececf3495ae00
Fernando Balieiro <fernandobalieiro@gmail.com>
2024-06-05 15:26:29 +0100

    Draft for RabbitMQ integration.

--- src/main/java/com/acme/MainApp.java
+++ src/main/java/com/acme/MainApp.java
@@ -1,5 +1,6 @@
 package com.acme;
 
+import org.apache.qpid.jms.JmsConnectionFactory;
 import org.apache.camel.main.Main;
 
 /**
@@ -11,9 +12,17 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-        Main main = new Main();
+        final var main = new Main();
+        main.bind("connectionFactory", connectionFactory());
         main.configure().addRoutesBuilder(new MyRouteBuilder());
         main.run(args);
+    }
+
+    private static JmsConnectionFactory connectionFactory() {
+        final var connectionFactory = new JmsConnectionFactory("amqp://localhost:5672");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("admin");
+        return connectionFactory;
     }
 
 }
